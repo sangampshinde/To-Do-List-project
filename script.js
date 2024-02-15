c66b69cb2ad9cf8766efceb26fce926dd5883af9
sangampshinde <sangampshinde@gmail.com>
2024-02-15 09:24:50 +0530

    js part complete except local storage

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ const item=document.querySelector('#item');
 const toDoBox=document.querySelector('#to-do-box');
 
 item.addEventListener(
-"keyup",
-function(event){
-    if(event.key == 'Enter')
-    {
-        addToDo(this.value);
-        this.value="";
+    "keyup",
+    function(event){
+       // console.log(event);
+       //console.log(event.key);
+
+        if(event.key == "Enter"){
+            // console.log(this.value);
+            addToDo(this.value);
+            this.value="";
+        }
     }
-}
-);
-const addToDo=(item)=>{
-    const listItem=document.createElement('li');
-    listItem.innerHTML=`
-    ${item}
-    <i class="fa fa-times"></i>
-    `
-    toDoBox
+)
+
+const addToDo= (item)=>{
+    const listItem= document.createElement('li');
+    listItem.innerHTML =`
+        ${item} 
+        <i class="fa fa-times"></i>
+    `;
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle('done');
+        }
+    );
+    listItem.querySelector('i').addEventListener('click', function(){
+        listItem.remove();
+    })
+    toDoBox.appendChild(listItem);
+
 }
